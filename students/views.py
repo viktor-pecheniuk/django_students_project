2e048bdb9b766dcbb277206efddfdae6e4354128
viktor <viktor.pecheniuk@gmail.com>
2014-12-27 13:53:12 +0200

    added dynamic url addresses

--- students/views.py
+++ students/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from django.template import RequestContext, loader
 
-# Create your views here.
+def students_list(request):
+	return render(request, 'students/students_list.html', {})
+
+def students_add(request):
+	return HttpResponse('<h1>Students Add Form</h1>')
+
+def students_edit(request, sid):
+	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request):
+	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+def groups_list(request):
+	return HttpResponse('<h1>Groups Listing</h1>')
+
+def groups_add(request):
+	return HttpResponse('<h1>Groups Add Form</h1>')
+
+def groups_edit(request, gid):
+	return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+	return HttpResponse('<h1>Delete Group %s</h1>' % gid)
